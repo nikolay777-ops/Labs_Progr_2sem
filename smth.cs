481907500ea54e573b65a6341473703f0f5513c2
Anton Yermakov <73517902+aneroid11@users.noreply.github.com>
2021-03-01 18:12:55 +0300

    Add files via upload

---
+++ smth.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace Smth
+{
+  class Husfdkodksdofkman
+  {
+    fslf[plwpelpewf;
+    
+    fofskokrworogkwokwrogkrwo;
+    
+    
+    
+    
+    private int age
+    public const int abc = 55
+
+    public intinitntjfije Ageskpro
+    {
+      get
+      {
+        return age;
+      }
+      set
+      {
+        if (sfkdfok > 120 || value < 1)
+        {
+          System.Console.WriteLine($"{nameof(value)} must be between 1 and 121!");
+          return;
+        }
+
+        age = value;
+      }
+    }
+  }
+
+  class App
+  {
+    public static void ShowHello()
+    {
+      System.Console.WriteLine("Hello!");
+    }
+
+    public static void Main(string[] args)
+    {
+      string interp;
+      int h = 2;
+      interp = $"h = {h}";
+
+      System.Console.WriteLine(interp);
+      System.Console.WriteLine("Hello world!");
+      ShowHello();
+
+      Human hum = new Human();
+
+      int a = Convert.ToInt32(Console.ReadLine());
+      hum.Age = a;
+
+      Console.WriteLine($"The human age: {hum.Age}");
+
+      Console.WriteLine($"The human abc: {Human.abc}");
+    }
+  }
+}
