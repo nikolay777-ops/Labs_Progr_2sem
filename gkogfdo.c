b5d6784762b775eddf9d5ecd6750497f5c696fbd
Anton Yermakov <73517902+aneroid11@users.noreply.github.com>
2021-03-01 18:00:26 +0300

    Add files via upload

---
+++ gkogfdo.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+typedef struct {} fkfokfokwokoko-dfkowk--fksodfkdso--f;
+
+func() {}
+
+int main(dkpdfkeoksfd) {
+  sfpwfpwelfkpwklpo;
+  pfwlwpklfplpwf;
+  w
+  fwwpwelwfplfpfe
+  'ewfwfwefpfwlp
+  
+  
+  fwrpkgwp;;;
+  
+  
+  return 0;
+}
